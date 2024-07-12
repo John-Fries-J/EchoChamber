a1acc5bd06d309e1bb6abebdf3366b3f738b922c
John Fries <w.bababooie@gmail.com>
2024-07-12 01:52:56 +0100

    Create feedback points command

---
+++ commands/utility/feedbackpoint.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { blue } = require('../../colors.json');
+
+const feedbackPointsFile = './feedbackpoints.json';
+
+let feedbackPoints = {};
+
+function loadFeedbackPoints() {
+    try {
+        feedbackPoints = JSON.parse(fs.readFileSync(feedbackPointsFile, 'utf-8'));
+    } catch (error) {
+        console.error('Error loading feedback points:', error);
+        feedbackPoints = {};
+    }
+}
+
+loadFeedbackPoints();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('feedback')
+		.setDescription('Replies with the user\'s points'),
+	async execute(interaction) {
+        const userId = interaction.user.id;
+        const userPoints = feedbackPoints[userId] || 0;
+
+		const embed = new EmbedBuilder()
+			.setTitle('Points')
+			.setDescription(`You currently have ${userPoints} points`)
+			.setColor(blue)
+			.setTimestamp();
+
+		await interaction.reply({ embeds: [embed] });
+	},
+};
