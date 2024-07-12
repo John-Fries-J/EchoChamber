63eac39f3ff08ce0a8792f75e9f0da84a67a7fe4
John Fries <w.bababooie@gmail.com>
2024-07-12 03:08:43 +0100

    fix points

---
+++ commands/utility/points.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { blue } = require('../../colors.json');
+const feedbackPointsFile = './feedbackpoints.json';
+let feedbackPoints = {};
+function loadFeedbackPoints() {
+    try {
+        feedbackPoints = JSON.parse(fs.readFileSync(feedbackPointsFile, 'utf-8'));
+    } catch (error) {
+        console.error('Error loading feedback points:', error);
+        feedbackPoints = {};
+    }
+}
+loadFeedbackPoints();
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('points')
+        .setDescription('Gets points of specified user')
+        .addUserOption(option => option.setName('user').setDescription('The user to view the points of')),
+    async execute(interaction) {
+        let userId;
+        const targetUser = interaction.options.getUser('user');
+        if (!targetUser) {
+            userId = interaction.user.id;
+        } else {
+            userId = targetUser.id;
+        }
+        if (!feedbackPoints[userId]) {
+            feedbackPoints[userId] = 0;
+        }
+        const embed = new EmbedBuilder()
+            .setTitle('Points')
+            .setDescription(`${targetUser ? targetUser.username : interaction.user.username} currently has ${feedbackPoints[userId]} points`)
+            .setColor(blue)
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed], ephemeral: true });
+    },
+};
